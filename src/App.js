82d2bbecd7587e1d7a7accaa4bb4886fc6eaae00
Nuttapon Achachotipong <nuttapon.a@linecorp.com>
2023-11-10 13:58:49 +0700

    feat: add Deployed on Github Action

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Nutty from app one
+          Nutty from app one (Deployed on Github Action)
         </a>
       </header>
     </div>
